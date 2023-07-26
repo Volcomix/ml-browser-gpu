cab79acfa06a8d7c0c93e9425093ed9b641b3256
Volcomix <Volcomix@users.noreply.github.com>
2023-07-26 09:51:50 +0200

    Bootstrap computing the entire layer 1 output

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,11 @@ const layer1WeightFragmentShader = /* glsl */ `#version 300 es
 
   void main() {
     ivec2 fragCoord = ivec2(gl_FragCoord);
-    ivec2 weightCoord = ivec2(fragCoord.y * 28 + fragCoord.x, 0);
+    // TODO Rearrange the weights before uploading to texture
+    ivec2 weightCoord = ivec2(
+      (fragCoord.x % 32) + (fragCoord.y % 32) * 28,
+      (fragCoord.x / 32) + (fragCoord.y / 32) * 23
+    );
     float x = texelFetch(xTex, fragCoord, 0).r;
     float weight = texelFetch(weightTex, weightCoord, 0).r;
     y = x * weight;
@@ -69,7 +73,7 @@ const layer1BiasFragmentShader = /* glsl */ `#version 300 es
     ivec2 fragCoord = ivec2(gl_FragCoord);
     float x = texelFetch(xTex, fragCoord, 5).r * 1024.0;
     float bias = texelFetch(biasTex, fragCoord, 0).r;
-    y = x + bias;
+    y = x + bias; // TODO Fix result
   }
 `
 
@@ -84,9 +88,19 @@ const layer1BiasProgramInfo = twgl.createProgramInfo(gl, [
 ])
 
 const arrays = {
+  // TODO Put faces only on pixels where we want to compute something
   inPosition: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
 }
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
+
+// 32 = lowest power of 2 that is greater than or equal to 28
+// 23 = ceil(sqrt(512))
+// 529 = 23 * 23
+// 736 = 32 * 23
+
+// TODO Refactor this rearrangement
+const layer1BiasReshaped = new Float32Array(529)
+layer1BiasReshaped.set(layer1Bias)
 
 type Textures = {
   x: WebGLTexture
@@ -98,7 +112,9 @@ const textures = await new Promise<Textures>((resolve) => {
   const result = twgl.createTextures(
     gl,
     {
+      // TODO Consider grouping 4 successive R components into single RGBA pixel
       x: { src: 'data/fashion-mnist/0.png' },
+      // TODO Set max mipmap level
       layer1Weight: {
         src: layer1Weight,
         internalFormat: gl.R32F,
@@ -106,10 +122,10 @@ const textures = await new Promise<Textures>((resolve) => {
         height: 512,
       },
       layer1Bias: {
-        src: layer1Bias,
+        src: layer1BiasReshaped,
         internalFormat: gl.R32F,
-        width: 512,
-        height: 1,
+        width: 23,
+        height: 23,
       },
     },
     () => resolve(result),
@@ -119,18 +135,18 @@ const textures = await new Promise<Textures>((resolve) => {
 const layer1WeightFrameBufferInfo = twgl.createFramebufferInfo(
   gl,
   [{ internalFormat: gl.R32F }],
-  32,
-  32,
+  736,
+  736,
 )
 
 const layer1BiasFrameBufferInfo = twgl.createFramebufferInfo(
   gl,
   [{ internalFormat: gl.R32F }],
-  1,
-  1,
+  23,
+  23,
 )
 
-gl.viewport(0, 0, 32, 32)
+gl.viewport(0, 0, 736, 736)
 
 const layer1WeightUniforms = {
   xTex: textures.x,
@@ -146,7 +162,7 @@ twgl.drawBufferInfo(gl, bufferInfo)
 gl.bindTexture(gl.TEXTURE_2D, layer1WeightFrameBufferInfo.attachments[0])
 gl.generateMipmap(gl.TEXTURE_2D)
 
-gl.viewport(0, 0, 1, 1)
+gl.viewport(0, 0, 23, 23)
 
 const layer1BiasUniforms = {
   xTex: layer1WeightFrameBufferInfo.attachments[0],
@@ -159,6 +175,7 @@ twgl.setBuffersAndAttributes(gl, layer1BiasProgramInfo, bufferInfo)
 twgl.setUniforms(layer1BiasProgramInfo, layer1BiasUniforms)
 twgl.drawBufferInfo(gl, bufferInfo)
 
-const hidden1 = new Float32Array(1)
-gl.readPixels(0, 0, 1, 1, gl.RED, gl.FLOAT, hidden1)
-console.log(`Hidden 1: ${hidden1}`)
+const hidden1 = new Float32Array(512)
+gl.readPixels(0, 0, 23, 22, gl.RED, gl.FLOAT, hidden1)
+gl.readPixels(0, 22, 6, 1, gl.RED, gl.FLOAT, hidden1, 506)
+console.log(`Hidden 1: ${hidden1.join(', ')}`)
