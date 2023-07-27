6611436a94156f7a6f493111961e0078dfa14b0f
Volcomix <Volcomix@users.noreply.github.com>
2023-07-27 20:46:37 +0200

    Implement hidden layer 3 in WebGL

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,20 @@ console.log(
     .join(', ')}, ...`,
 )
 
+const layer2Weight = await fetchParameter('layer2-weight')
+console.log(
+  `Layer 2 weight: ${[...layer2Weight.slice(0, 3)]
+    .map((value) => value.toFixed(4))
+    .join(', ')}, ...`,
+)
+
+const layer2BiasRaw = await fetchParameter('layer2-bias')
+console.log(
+  `Layer 2 bias: ${[...layer2BiasRaw.slice(0, 3)]
+    .map((value) => value.toFixed(7))
+    .join(', ')}, ...`,
+)
+
 const canvas = new OffscreenCanvas(1, 1)
 const gl = canvas.getContext('webgl2')
 if (!gl) {
@@ -77,6 +91,45 @@ const layer0BiasLayer1FragmentShader = /* glsl */ `#version 300 es
   }
 `
 
+const layer2WeightFragmentShader = /* glsl */ `#version 300 es
+
+  precision highp float;
+  
+  uniform sampler2D xTex;
+  uniform sampler2D weightTex;
+
+  out float y;
+
+  void main() {
+    ivec2 fragCoord = ivec2(gl_FragCoord);
+    // TODO Rearrange the weights before uploading to texture
+    ivec2 weightCoord = ivec2(
+      (fragCoord.x % 32) + (fragCoord.y % 32) * 23,
+      (fragCoord.x / 32) + (fragCoord.y / 32) * 23
+    );
+    float x = texelFetch(xTex, fragCoord % 32, 0).r;
+    float weight = texelFetch(weightTex, weightCoord, 0).r;
+    y = x * weight;
+  }
+`
+
+const layer2BiasLayer3FragmentShader = /* glsl */ `#version 300 es
+
+  precision highp float;
+
+  uniform sampler2D xTex;
+  uniform sampler2D biasTex;
+
+  out float y;
+
+  void main() {
+    ivec2 fragCoord = ivec2(gl_FragCoord);
+    float x = texelFetch(xTex, fragCoord, 5).r * 1024.0;
+    float bias = texelFetch(biasTex, fragCoord, 0).r;
+    y = max(0.0, x + bias); // ReLU
+  }
+`
+
 const layer0WeightProgramInfo = twgl.createProgramInfo(gl, [
   vertexShader,
   layer0WeightFragmentShader,
@@ -85,6 +138,16 @@ const layer0WeightProgramInfo = twgl.createProgramInfo(gl, [
 const layer0BiasLayer1ProgramInfo = twgl.createProgramInfo(gl, [
   vertexShader,
   layer0BiasLayer1FragmentShader,
+])
+
+const layer2WeightProgramInfo = twgl.createProgramInfo(gl, [
+  vertexShader,
+  layer2WeightFragmentShader,
+])
+
+const layer2BiasLayer3ProgramInfo = twgl.createProgramInfo(gl, [
+  vertexShader,
+  layer2BiasLayer3FragmentShader,
 ])
 
 const arrays = {
@@ -101,10 +164,15 @@ const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 const layer0Bias = new Float32Array(529)
 layer0Bias.set(layer0BiasRaw)
 
+const layer2Bias = new Float32Array(529)
+layer2Bias.set(layer2BiasRaw)
+
 type Textures = {
   x: WebGLTexture
   layer0Weight: WebGLTexture
   layer0Bias: WebGLTexture
+  layer2Weight: WebGLTexture
+  layer2Bias: WebGLTexture
 }
 
 const textures = await new Promise<Textures>((resolve) => {
@@ -126,6 +194,18 @@ const textures = await new Promise<Textures>((resolve) => {
         width: 23,
         height: 23,
       },
+      layer2Weight: {
+        src: layer2Weight,
+        internalFormat: gl.R32F,
+        width: 512,
+        height: 512,
+      },
+      layer2Bias: {
+        src: layer2Bias,
+        internalFormat: gl.R32F,
+        width: 23,
+        height: 23,
+      },
     },
     () => resolve(result),
   ) as Textures
@@ -139,6 +219,20 @@ const layer0WeightFrameBufferInfo = twgl.createFramebufferInfo(
 )
 
 const layer0BiasLayer1FrameBufferInfo = twgl.createFramebufferInfo(
+  gl,
+  [{ internalFormat: gl.R32F }],
+  23,
+  23,
+)
+
+const layer2WeightFrameBufferInfo = twgl.createFramebufferInfo(
+  gl,
+  [{ internalFormat: gl.R32F }],
+  736,
+  736,
+)
+
+const layer2BiasLayer3FrameBufferInfo = twgl.createFramebufferInfo(
   gl,
   [{ internalFormat: gl.R32F }],
   23,
@@ -179,6 +273,44 @@ gl.readPixels(0, 0, 23, 22, gl.RED, gl.FLOAT, hidden1)
 gl.readPixels(0, 22, 6, 1, gl.RED, gl.FLOAT, hidden1, 506)
 console.log(
   `Hidden 1: ${hidden1.slice(0, 10).join(', ')}, ... , ${hidden1
+    .slice(-10)
+    .join(', ')}`,
+)
+
+gl.viewport(0, 0, 736, 736)
+
+const layer2WeightUniforms = {
+  xTex: layer0BiasLayer1FrameBufferInfo.attachments[0],
+  weightTex: textures.layer2Weight,
+}
+
+gl.bindFramebuffer(gl.FRAMEBUFFER, layer2WeightFrameBufferInfo.framebuffer)
+gl.useProgram(layer2WeightProgramInfo.program)
+twgl.setBuffersAndAttributes(gl, layer2WeightProgramInfo, bufferInfo)
+twgl.setUniforms(layer2WeightProgramInfo, layer2WeightUniforms)
+twgl.drawBufferInfo(gl, bufferInfo)
+
+gl.bindTexture(gl.TEXTURE_2D, layer2WeightFrameBufferInfo.attachments[0])
+gl.generateMipmap(gl.TEXTURE_2D)
+
+gl.viewport(0, 0, 23, 23)
+
+const layer2BiasLayer3Uniforms = {
+  xTex: layer2WeightFrameBufferInfo.attachments[0],
+  biasTex: textures.layer2Bias,
+}
+
+gl.bindFramebuffer(gl.FRAMEBUFFER, layer2BiasLayer3FrameBufferInfo.framebuffer)
+gl.useProgram(layer2BiasLayer3ProgramInfo.program)
+twgl.setBuffersAndAttributes(gl, layer2BiasLayer3ProgramInfo, bufferInfo)
+twgl.setUniforms(layer2BiasLayer3ProgramInfo, layer2BiasLayer3Uniforms)
+twgl.drawBufferInfo(gl, bufferInfo)
+
+const hidden3 = new Float32Array(512)
+gl.readPixels(0, 0, 23, 22, gl.RED, gl.FLOAT, hidden3)
+gl.readPixels(0, 22, 6, 1, gl.RED, gl.FLOAT, hidden3, 506)
+console.log(
+  `Hidden 3: ${hidden3.slice(0, 10).join(', ')}, ... , ${hidden3
     .slice(-10)
     .join(', ')}`,
 )
