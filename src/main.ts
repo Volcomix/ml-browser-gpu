6e15b300dae85d0080a2852e3d17dbe0887c77e8
Volcomix <Volcomix@users.noreply.github.com>
2023-07-30 18:36:30 +0200

    Finish inference implementation

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ if (!gl) {
 twgl.addExtensionsToContext(gl)
 
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, {
-  // TODO Check if possible optimizations by changing the faces
   inPosition: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
 })
 
@@ -61,11 +60,7 @@ const process = (
 const fetchParameter = async (name: string) => {
   const response = await fetch(`models/fashion-mnist/${name}.gz`)
   const buffer = await response.arrayBuffer()
-  const result = new Float32Array(buffer)
-
-  console.log(`${name}: ${[...result.slice(0, 3)].join(', ')}, ...`)
-
-  return result
+  return new Float32Array(buffer)
 }
 
 type Size = [number, number]
@@ -175,6 +170,11 @@ const createFramebufferInfos = (): FramebufferInfos => {
   }
 }
 
+type Activation = (x: string) => string
+
+const Identity = (x: string) => x
+const ReLU = (x: string) => `max(vec4(0), ${x})`
+
 const setupMultiply1D = (tileWidth: number, tileHeight: number) => {
   const fragmentShader = /* glsl */ `#version 300 es
 
@@ -243,7 +243,11 @@ const setupMultiply2D = (tileHeight: number) => {
   return multiply2D
 }
 
-const setupSum1DReLU = (tileSize: number, lod: number) => {
+const setupSum1D = (
+  tileSize: number,
+  lod: number,
+  activation: Activation = Identity,
+) => {
   const fragmentShader = /* glsl */ `#version 300 es
 
     precision highp float;
@@ -257,13 +261,13 @@ const setupSum1DReLU = (tileSize: number, lod: number) => {
       ivec2 fragCoord = ivec2(gl_FragCoord);
       vec4 x = texelFetch(xTex, fragCoord, ${lod}) * ${tileSize}.0;
       vec4 b = texelFetch(bTex, fragCoord, 0);
-      y = max(vec4(0), x + b); // ReLU
+      y = ${activation('x + b')};
     }
   `
 
   const programInfo = twgl.createProgramInfo(gl, [vertexShader, fragmentShader])
 
-  const sum1DReLU = (
+  const sum1D = (
     xTex: WebGLTexture,
     bTex: WebGLTexture,
     fbi: FramebufferInfo,
@@ -276,11 +280,16 @@ const setupSum1DReLU = (tileSize: number, lod: number) => {
     process(programInfo, fbi, { xTex, bTex }, viewportWidth, viewportHeight)
   }
 
-  return sum1DReLU
+  return sum1D
 }
 
 // TODO Improve contract
-const setupSum2DReLU = (tileSize: number, tileCount: number, lod: number) => {
+const setupSum2D = (
+  tileSize: number,
+  tileCount: number,
+  lod: number,
+  activation: Activation = Identity,
+) => {
   const fragmentShader = /* glsl */ `#version 300 es
 
     precision highp float;
@@ -297,13 +306,13 @@ const setupSum2DReLU = (tileSize: number, tileCount: number, lod: number) => {
         (_v, k) => `texelFetch(xTex, ivec2(${k}, fragCoord.y), ${lod})`,
       ).join(' + ')}) * ${tileSize / tileCount}.0;
       vec4 b = texelFetch(bTex, fragCoord, 0);
-      y = max(vec4(0), x + b); // ReLU
+      y = ${activation('x + b')};
     }
   `
 
   const programInfo = twgl.createProgramInfo(gl, [vertexShader, fragmentShader])
 
-  const sum2DReLU = (
+  const sum2D = (
     xTex: WebGLTexture,
     bTex: WebGLTexture,
     fbi: FramebufferInfo,
@@ -316,11 +325,22 @@ const setupSum2DReLU = (tileSize: number, tileCount: number, lod: number) => {
     process(programInfo, fbi, { xTex, bTex }, viewportWidth, viewportHeight)
   }
 
-  return sum2DReLU
+  return sum2D
 }
 
-// TODO Remove linter ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const argmax = (x: Float32Array) => {
+  return [...x].reduce(
+    (acc, value, index) => (value > acc.max ? { max: value, index } : acc),
+    { max: -Infinity, index: -1 },
+  ).index
+}
+
+const format = (data: Float32Array, maximumFractionDigits: number) => {
+  return [...data]
+    .map((value) => value.toLocaleString('en-US', { maximumFractionDigits }))
+    .join(', ')
+}
+
 const [weight0, bias0, weight2, bias2, weight4, bias4, input] =
   await Promise.all([
     loadWeight('0-weight', {
@@ -353,272 +373,42 @@ const fbi = createFramebufferInfos()
 
 const multiply2D = setupMultiply2D(32)
 const multiply1D = setupMultiply1D(32, 16)
-const sum1DReLU = setupSum1DReLU(1024, 5)
-const sum2DReLU = setupSum2DReLU(512, 2, 4)
+const sum1DReLU = setupSum1D(1024, 5, ReLU)
+const sum2DReLU = setupSum2D(512, 2, 4, ReLU)
+const sum2D = setupSum2D(512, 2, 4)
+
+const classes = [
+  'T-shirt/top',
+  'Trouser',
+  'Pullover',
+  'Dress',
+  'Coat',
+  'Sandal',
+  'Shirt',
+  'Sneaker',
+  'Bag',
+  'Ankle boot',
+]
+
+const start = performance.now()
 
 multiply2D(input, weight0, fbi['32x4096'], 28, 4092)
 sum1DReLU(fbi['32x4096'].attachment, bias0, fbi['1x128'], 1, 128)
 multiply1D(fbi['1x128'].attachment, weight2, fbi['32x4096'], 32, 2048)
 sum2DReLU(fbi['32x4096'].attachment, bias2, fbi['1x128'], 1, 128)
+multiply1D(fbi['1x128'].attachment, weight4, fbi['32x4096'], 32, 48)
+sum2D(fbi['32x4096'].attachment, bias4, fbi['1x128'], 1, 3)
 
-if (Number(1)) {
-  throw new Error('Implementation not finished')
-}
+const gpuTime = performance.now() - start
 
-const layer0WeightFragmentShader = /* glsl */ `#version 300 es
+const outputData = new Float32Array(12)
+gl.readPixels(0, 0, 1, 3, gl.RGBA, gl.FLOAT, outputData)
+const output = outputData.slice(0, 10)
+const predicted = classes[argmax(output)]
 
-  precision highp float;
-  
-  uniform sampler2D xTex;
-  uniform sampler2D weightTex;
+const totalTime = performance.now() - start
 
-  out float y;
-
-  void main() {
-    ivec2 fragCoord = ivec2(gl_FragCoord);
-    // TODO Rearrange the weights before uploading to texture
-    ivec2 weightCoord = ivec2(
-      (fragCoord.x % 32) + (fragCoord.y % 32) * 28,
-      (fragCoord.x / 32) + (fragCoord.y / 32) * 23
-    );
-    float x = texelFetch(xTex, fragCoord % 32, 0).r;
-    float weight = texelFetch(weightTex, weightCoord, 0).r;
-    y = x * weight;
-  }
-`
-
-const layer0BiasLayer1FragmentShader = /* glsl */ `#version 300 es
-
-  precision highp float;
-
-  uniform sampler2D xTex;
-  uniform sampler2D biasTex;
-
-  out float y;
-
-  void main() {
-    ivec2 fragCoord = ivec2(gl_FragCoord);
-    float x = texelFetch(xTex, fragCoord, 5).r * 1024.0;
-    float bias = texelFetch(biasTex, fragCoord, 0).r;
-    y = max(0.0, x + bias); // ReLU
-  }
-`
-
-const layer2WeightFragmentShader = /* glsl */ `#version 300 es
-
-  precision highp float;
-  
-  uniform sampler2D xTex;
-  uniform sampler2D weightTex;
-
-  out float y;
-
-  void main() {
-    ivec2 fragCoord = ivec2(gl_FragCoord);
-    // TODO Rearrange the weights before uploading to texture
-    ivec2 weightCoord = ivec2(
-      (fragCoord.x % 32) + (fragCoord.y % 32) * 23,
-      (fragCoord.x / 32) + (fragCoord.y / 32) * 23
-    );
-    float x = texelFetch(xTex, fragCoord % 32, 0).r;
-    float weight = texelFetch(weightTex, weightCoord, 0).r;
-    y = x * weight;
-  }
-`
-
-const layer2BiasLayer3FragmentShader = /* glsl */ `#version 300 es
-
-  precision highp float;
-
-  uniform sampler2D xTex;
-  uniform sampler2D biasTex;
-
-  out float y;
-
-  void main() {
-    ivec2 fragCoord = ivec2(gl_FragCoord);
-    float x = texelFetch(xTex, fragCoord, 5).r * 1024.0;
-    float bias = texelFetch(biasTex, fragCoord, 0).r;
-    y = max(0.0, x + bias); // ReLU
-  }
-`
-
-const layer0WeightProgramInfo = twgl.createProgramInfo(gl, [
-  vertexShader,
-  layer0WeightFragmentShader,
-])
-
-const layer0BiasLayer1ProgramInfo = twgl.createProgramInfo(gl, [
-  vertexShader,
-  layer0BiasLayer1FragmentShader,
-])
-
-const layer2WeightProgramInfo = twgl.createProgramInfo(gl, [
-  vertexShader,
-  layer2WeightFragmentShader,
-])
-
-const layer2BiasLayer3ProgramInfo = twgl.createProgramInfo(gl, [
-  vertexShader,
-  layer2BiasLayer3FragmentShader,
-])
-
-// 32 = lowest power of 2 that is greater than or equal to 28
-// 23 = ceil(sqrt(512))
-// 529 = 23 * 23
-// 736 = 32 * 23
-
-const layer0Bias = new Float32Array(529)
-layer0Bias.set(bias0Raw)
-
-const layer2Bias = new Float32Array(529)
-layer2Bias.set(biasRaw2)
-
-type Textures = {
-  x: WebGLTexture
-  layer0Weight: WebGLTexture
-  layer0Bias: WebGLTexture
-  layer2Weight: WebGLTexture
-  layer2Bias: WebGLTexture
-}
-
-const textures = await new Promise<Textures>((resolve) => {
-  const result = twgl.createTextures(
-    gl,
-    {
-      // TODO Consider grouping 4 successive R components into single RGBA pixel
-      x: { src: 'data/fashion-mnist/0.png' },
-      // TODO Set max mipmap level
-      layer0Weight: {
-        src: weight0,
-        internalFormat: gl.R32F,
-        width: 784,
-        height: 512,
-      },
-      layer0Bias: {
-        src: layer0Bias,
-        internalFormat: gl.R32F,
-        width: 23,
-        height: 23,
-      },
-      layer2Weight: {
-        src: weight2,
-        internalFormat: gl.R32F,
-        width: 512,
-        height: 512,
-      },
-      layer2Bias: {
-        src: layer2Bias,
-        internalFormat: gl.R32F,
-        width: 23,
-        height: 23,
-      },
-    },
-    () => resolve(result),
-  ) as Textures
-})
-
-const layer0WeightFrameBufferInfo = twgl.createFramebufferInfo(
-  gl,
-  [{ internalFormat: gl.R32F }],
-  736,
-  736,
-)
-
-const layer0BiasLayer1FrameBufferInfo = twgl.createFramebufferInfo(
-  gl,
-  [{ internalFormat: gl.R32F }],
-  23,
-  23,
-)
-
-const layer2WeightFrameBufferInfo = twgl.createFramebufferInfo(
-  gl,
-  [{ internalFormat: gl.R32F }],
-  736,
-  736,
-)
-
-const layer2BiasLayer3FrameBufferInfo = twgl.createFramebufferInfo(
-  gl,
-  [{ internalFormat: gl.R32F }],
-  23,
-  23,
-)
-
-gl.viewport(0, 0, 736, 736)
-
-const layer0WeightUniforms = {
-  xTex: textures.x,
-  weightTex: textures.layer0Weight,
-}
-
-gl.bindFramebuffer(gl.FRAMEBUFFER, layer0WeightFrameBufferInfo.framebuffer)
-gl.useProgram(layer0WeightProgramInfo.program)
-twgl.setBuffersAndAttributes(gl, layer0WeightProgramInfo, bufferInfo)
-twgl.setUniforms(layer0WeightProgramInfo, layer0WeightUniforms)
-twgl.drawBufferInfo(gl, bufferInfo)
-
-gl.bindTexture(gl.TEXTURE_2D, layer0WeightFrameBufferInfo.attachments[0])
-gl.generateMipmap(gl.TEXTURE_2D)
-
-gl.viewport(0, 0, 23, 23)
-
-const layer0BiasLayer1Uniforms = {
-  xTex: layer0WeightFrameBufferInfo.attachments[0],
-  biasTex: textures.layer0Bias,
-}
-
-gl.bindFramebuffer(gl.FRAMEBUFFER, layer0BiasLayer1FrameBufferInfo.framebuffer)
-gl.useProgram(layer0BiasLayer1ProgramInfo.program)
-twgl.setBuffersAndAttributes(gl, layer0BiasLayer1ProgramInfo, bufferInfo)
-twgl.setUniforms(layer0BiasLayer1ProgramInfo, layer0BiasLayer1Uniforms)
-twgl.drawBufferInfo(gl, bufferInfo)
-
-// FIXME Error on Firefox
-const hidden1 = new Float32Array(512)
-gl.readPixels(0, 0, 23, 22, gl.RED, gl.FLOAT, hidden1)
-gl.readPixels(0, 22, 6, 1, gl.RED, gl.FLOAT, hidden1, 506)
-console.log(
-  `Hidden 1: ${hidden1.slice(0, 10).join(', ')}, ... , ${hidden1
-    .slice(-10)
-    .join(', ')}`,
-)
-
-gl.viewport(0, 0, 736, 736)
-
-const layer2WeightUniforms = {
-  xTex: layer0BiasLayer1FrameBufferInfo.attachments[0],
-  weightTex: textures.layer2Weight,
-}
-
-gl.bindFramebuffer(gl.FRAMEBUFFER, layer2WeightFrameBufferInfo.framebuffer)
-gl.useProgram(layer2WeightProgramInfo.program)
-twgl.setBuffersAndAttributes(gl, layer2WeightProgramInfo, bufferInfo)
-twgl.setUniforms(layer2WeightProgramInfo, layer2WeightUniforms)
-twgl.drawBufferInfo(gl, bufferInfo)
-
-gl.bindTexture(gl.TEXTURE_2D, layer2WeightFrameBufferInfo.attachments[0])
-gl.generateMipmap(gl.TEXTURE_2D)
-
-gl.viewport(0, 0, 23, 23)
-
-const layer2BiasLayer3Uniforms = {
-  xTex: layer2WeightFrameBufferInfo.attachments[0],
-  biasTex: textures.layer2Bias,
-}
-
-gl.bindFramebuffer(gl.FRAMEBUFFER, layer2BiasLayer3FrameBufferInfo.framebuffer)
-gl.useProgram(layer2BiasLayer3ProgramInfo.program)
-twgl.setBuffersAndAttributes(gl, layer2BiasLayer3ProgramInfo, bufferInfo)
-twgl.setUniforms(layer2BiasLayer3ProgramInfo, layer2BiasLayer3Uniforms)
-twgl.drawBufferInfo(gl, bufferInfo)
-
-const hidden3 = new Float32Array(512)
-gl.readPixels(0, 0, 23, 22, gl.RED, gl.FLOAT, hidden3)
-gl.readPixels(0, 22, 6, 1, gl.RED, gl.FLOAT, hidden3, 506)
-console.log(
-  `Hidden 3: ${hidden3.slice(0, 10).join(', ')}, ... , ${hidden3
-    .slice(-10)
-    .join(', ')}`,
-)
+console.log(`Output: ${format(output, 4)}`)
+console.log(`Predicted: ${predicted}`)
+console.log(`GPU time: ${gpuTime}ms`)
+console.log(`Total time: ${totalTime}ms`)
