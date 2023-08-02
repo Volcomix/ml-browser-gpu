691b868e70b2712dfd5596846af406d7afaac199
Volcomix <Volcomix@users.noreply.github.com>
2023-08-02 20:36:21 +0200

    Bootstrap weight loading with WebGPU

--- src/fashion-mnist-webgpu.ts
+++ src/fashion-mnist-webgpu.ts
@@ -11,63 +11,106 @@ if (!adapter) {
 
 const device = await adapter.requestDevice()
 
-const count = 4
-const bufferSize = count * 4
+const loadParameter = async (name: string) => {
+  const response = await fetch(`models/fashion-mnist/${name}.gz`)
+  const arrayBuffer = await response.arrayBuffer()
 
-const module = device.createShaderModule({
-  code: /* wgsl */ `
-    @group(0) @binding(0)
-    var<storage, read_write> data: array<f32>;
+  const buffer = device.createBuffer({
+    size: arrayBuffer.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  })
+  device.queue.writeBuffer(buffer, 0, arrayBuffer)
 
-    @compute @workgroup_size(1)
-    fn computeSomething(
-      @builtin(global_invocation_id)
-      id: vec3u
-    ) {
-      data[id.x] = f32(id.x) * 2.0;
-    }
-  `,
-})
+  return buffer
+}
 
-const pipeline = device.createComputePipeline({
-  layout: 'auto',
-  compute: {
-    module,
-    entryPoint: 'computeSomething',
-  },
-})
+const [weight0] = await Promise.all([loadParameter('0-weight')])
 
-const output = device.createBuffer({
-  size: bufferSize,
+const result = device.createBuffer({
+  size: weight0.size,
   usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
 })
 
 const stagingBuffer = device.createBuffer({
-  size: bufferSize,
+  size: 8 * 4,
   usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
 })
 
-const bindGroup = device.createBindGroup({
-  layout: pipeline.getBindGroupLayout(0),
-  entries: [{ binding: 0, resource: { buffer: output } }],
+const module = device.createShaderModule({
+  code: /* wgsl */ `
+    @group(0) @binding(0)
+    var<storage> weight: array<f32>;
+
+    @group(1) @binding(0)
+    var<storage, read_write> result: array<f32>;
+
+    @compute @workgroup_size(1)
+    fn main(
+      @builtin(global_invocation_id)
+      id: vec3u
+    ) {
+      result[id.x] = weight[id.x];
+    }
+  `,
+})
+
+const parameterBindGroupLayout = device.createBindGroupLayout({
+  entries: [
+    {
+      binding: 0,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: { type: 'read-only-storage' },
+    },
+  ],
+})
+
+const parameterBindGroup = device.createBindGroup({
+  layout: parameterBindGroupLayout,
+  entries: [{ binding: 0, resource: { buffer: weight0 } }],
+})
+
+const resultBindGroupLayout = device.createBindGroupLayout({
+  entries: [
+    {
+      binding: 0,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: { type: 'storage' },
+    },
+  ],
+})
+
+const resultBindGroup = device.createBindGroup({
+  layout: resultBindGroupLayout,
+  entries: [{ binding: 0, resource: { buffer: result } }],
+})
+
+const pipelineLayout = device.createPipelineLayout({
+  bindGroupLayouts: [parameterBindGroupLayout, resultBindGroupLayout],
+})
+
+const pipeline = device.createComputePipeline({
+  layout: pipelineLayout,
+  compute: {
+    module,
+    entryPoint: 'main',
+  },
 })
 
 const encoder = device.createCommandEncoder()
 
 const pass = encoder.beginComputePass()
 pass.setPipeline(pipeline)
-pass.setBindGroup(0, bindGroup)
-pass.dispatchWorkgroups(count)
+pass.setBindGroup(0, parameterBindGroup)
+pass.setBindGroup(1, resultBindGroup)
+pass.dispatchWorkgroups(4)
 pass.end()
 
-encoder.copyBufferToBuffer(output, 0, stagingBuffer, 0, bufferSize)
+encoder.copyBufferToBuffer(result, 0, stagingBuffer, 0, 8 * 4)
 
 const commands = encoder.finish()
 device.queue.submit([commands])
 
 await stagingBuffer.mapAsync(GPUMapMode.READ)
-const result = new Float32Array(stagingBuffer.getMappedRange())
-
-console.log(`Result: ${result}`)
+console.log(`Result: ${new Float32Array(stagingBuffer.getMappedRange())}`)
 
 stagingBuffer.unmap()
